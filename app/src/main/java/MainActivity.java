5af4bad97ba31ac58dd4c037af0bea52f6508501
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-20 19:16:15 +1000

    Update MainActivity.java

--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,1 +1,32 @@
+package com.example.tollnavigator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnViewSites, btnUploadSites;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnViewSites = findViewById(R.id.btnViewSites);
+        btnUploadSites = findViewById(R.id.btnUploadSites);
+
+        btnViewSites.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SiteListActivity.class);
+            startActivity(intent);
+        });
+
+        btnUploadSites.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, UploadActivity.class);
+            startActivity(intent);
+        });
+    }
+}
 
